67f26afff029f50472483a4bb7a48d3de9f41799
tolstovrob <tolstovrob@gmail.com>
2025-01-24 14:10:08 +0400

    feat(templates): add Course Overview page

---
+++ src/lib/modules/CourseCard.svelte
@@ -0,0 +1,50 @@
+<script
+  lang="ts"
+  context="module">
+  export interface CourseCardProps {
+    id: number;
+    icon: ComponentType<Icon>;
+    title: string;
+    teachers: { id: number; lastname: string; firstname?: string; patronymic?: string }[];
+  }
+</script>
+
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card';
+  import { Button } from '$lib/components/ui/button';
+  import type { ComponentType } from 'svelte';
+  import type { Icon } from 'lucide-svelte';
+
+  export let course: CourseCardProps;
+</script>
+
+<a href={`/courses/${course.id}`}>
+  <Card.Root
+    class="group relative flex min-h-36 flex-col justify-between overflow-hidden transition-all hover:bg-accent">
+    <div class="absolute -right-10 z-0 opacity-5 transition-all group-hover:-rotate-[15deg]">
+      <svelte:component
+        this={course.icon}
+        class="h-[150%]"
+        size="128" />
+    </div>
+    <Card.Header>
+      <Card.Title class="text-2xl leading-tight">
+        <span class="max-w-[75%]">{course.title}</span>
+      </Card.Title>
+    </Card.Header>
+    <Card.Footer class="flex flex-wrap gap-2">
+      {#each course.teachers as teacher, index}
+        <Button
+          href={`/users/${teacher.id}`}
+          variant="link"
+          size="sm"
+          class="-my-1 h-auto p-0 text-sm text-zinc-500">
+          {teacher.lastname}
+          {teacher.firstname ? teacher.firstname[0] + '.' : ''}
+          {teacher.firstname && teacher.patronymic ? teacher.patronymic[0] + '.' : ''}
+          {index + 1 !== course.teachers.length ? ',' : ''}
+        </Button>
+      {/each}
+    </Card.Footer>
+  </Card.Root>
+</a>
---
+++ src/routes/courses/+page.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
+  import Skeleton from '$lib/components/ui/skeleton/skeleton.svelte';
+  import CourseCard, { type CourseCardProps } from '$lib/modules/CourseCard.svelte';
+  import {
+    SquareTerminalIcon,
+    DatabaseIcon,
+    ShapesIcon,
+    CpuIcon,
+    SchoolIcon,
+    RotateCcwIcon,
+  } from 'lucide-svelte';
+
+  const coursesMocks: CourseCardProps[] = [
+    {
+      id: 1,
+      icon: SquareTerminalIcon,
+      title: 'СИТ 2025',
+      teachers: [
+        {
+          id: 1,
+          lastname: 'Рыданов',
+          firstname: 'Никита',
+          patronymic: 'Сергеевич',
+        },
+        {
+          id: 2,
+          lastname: 'Чернигин',
+          firstname: 'Михаил',
+          patronymic: 'Андреевич',
+        },
+        {
+          id: 3,
+          lastname: 'Гущин',
+          firstname: 'Андрей',
+        },
+      ],
+    },
+    {
+      id: 2,
+      icon: ShapesIcon,
+      title: 'Структуры данных и алгоритмы',
+      teachers: [
+        {
+          id: 4,
+          lastname: 'Батраева',
+          firstname: 'Инна',
+          patronymic: 'Александровна',
+        },
+        {
+          id: 5,
+          lastname: 'Векслер',
+          firstname: 'Виталий',
+          patronymic: 'Абрамович',
+        },
+      ],
+    },
+    {
+      id: 3,
+      icon: CpuIcon,
+      title: 'Операционные системы',
+      teachers: [
+        {
+          id: 6,
+          lastname: 'Соловьёв',
+          firstname: 'Владимир',
+          patronymic: 'Михайлович',
+        },
+      ],
+    },
+    {
+      id: 4,
+      icon: DatabaseIcon,
+      title: 'Системы управления базами данных',
+      teachers: [
+        {
+          id: 4,
+          lastname: 'Батраева',
+          firstname: 'Инна',
+          patronymic: 'Александровна',
+        },
+      ],
+    },
+    {
+      id: 5,
+      icon: SchoolIcon,
+      title: 'Введение в специальность',
+      teachers: [
+        {
+          id: 7,
+          lastname: 'Кудрина',
+          firstname: 'Елена',
+          patronymic: 'Вячеславовна',
+        },
+      ],
+    },
+  ];
+
+  let courses: CourseCardProps[] = [];
+  let loading = true;
+  let error: string | null = null;
+
+  const fetchCourses = async (): Promise<void> => {
+    loading = true;
+    error = null;
+    return new Promise<void>((resolve) =>
+      setTimeout(() => {
+        loading = false;
+        coursesMocks.forEach((element) => courses.push(element));
+        resolve();
+      }, 1000),
+    );
+  };
+
+  fetchCourses();
+</script>
+
+<h1 class="mt-6 text-5xl font-bold">Курсы</h1>
+
+<div class="my-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+  {#if loading}
+    <Skeleton class="min-h-36" />
+    <Skeleton class="min-h-36" />
+    <Skeleton class="min-h-36" />
+    <Skeleton class="min-h-36" />
+    <Skeleton class="min-h-36" />
+    <Skeleton class="min-h-36" />
+  {:else if error}
+    <div class="flex flex-row items-center gap-4">
+      <span class="text-lg text-destructive">
+        Не удалось загрузить курсы: {error}
+      </span>
+      <Button
+        size="icon"
+        variant="ghost"
+        on:click={fetchCourses}>
+        <RotateCcwIcon />
+      </Button>
+    </div>
+  {:else}
+    {#each courses as course}
+      <CourseCard {course} />
+    {:else}
+      <div>Курсы не найдены. Попробуйте убрать фильтры</div>
+    {/each}
+  {/if}
+</div>
